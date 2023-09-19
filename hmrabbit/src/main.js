bcefce4d37f48b8950100ab8a3e05c32edc294f0
twq46 <1430953131@qq.com>
2023-09-19 20:58:59 +0800

    项目初始化

--- hmrabbit/src/main.js
+++ hmrabbit/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+//1.导入createPinia
+import {createPinia} from 'pinia'
 
-createApp(App).mount('#app')
+//2.执行方法得到的实例
+const pinia = createPinia()
+
+//3.把pinia实例加入到app应用中
+createApp(App).use(pinia).use(router).mount('#app')
